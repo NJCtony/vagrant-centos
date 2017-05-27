f9c780a8b0f56d32d99551e8e3588d1c5b0db1f9
Shi Ping <limshiping93@hotmail.com>
2017-05-27 16:43:06 +0800

    Models: amended field lengths to accomodate data

--- src/dashboard/models.py
+++ src/dashboard/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class OrderDifference(models.Model):
-    clm_code = models.CharField(max_length=16, default='')
-    soldtoname = models.CharField(max_length=16, default='')
-    salesname = models.CharField(max_length=16, default='')
-    monat = models.CharField(max_length=16, default='')
+    clm_code = models.CharField(max_length=16, default=None)
+    soldtoname = models.CharField(max_length=64, default=None)
+    salesname = models.CharField(max_length=32, default=None)
+    monat = models.CharField(max_length=16, default=None)
     akt_day = models.DateField()
     last_umwteuro_amt = models.FloatField(default=0)
     this_umwteuro_amt = models.FloatField(default=0)
@@ -15,5 +15,11 @@ class OrderDifference(models.Model):
     diff_umwtpcs = models.IntegerField(default=0)
     diff_umwtpcs_percent = models.FloatField(default=0)
     sc_diff_umwteuro_percent = models.FloatField(default=0)
-    alert_type = models.CharField(max_length=16, default='')
-    alert_description = models.CharField(max_length=64, default='')
+    alert_type = models.CharField(max_length=16, default=None)
+    alert_description = models.CharField(max_length=64, default=None)
+
+class BusinessPerformance(models.Model):
+    clm_code = models.CharField(max_length=16, default=None)
+    soldtoname = models.CharField(max_length=64, default=None)
+    bp = models.FloatField(default=0)
+    monat = models.CharField(max_length=32, default=None)
