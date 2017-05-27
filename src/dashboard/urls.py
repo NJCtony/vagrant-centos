68b27e38c928da161361badb14e27f6d3eb76323
Sean <sean_lim@mymail.sutd.edu.sg>
2017-05-27 16:28:28 +0800

    Added: Login page (not functional yet)

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'dashboard' # namespacing to allow URL ReverseMatch
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/', views.login, name='login'),
 ]
--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,9 +1,32 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# Security
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.shortcuts import redirect
 
 def index(request):
     context={}
     # if not request.user.is_authenticated:
-    #     return redirect('mainapp:login')
+    #     return redirect('dashboard:login')
     return render(request, 'dashboard/index.html', context)
+
+def login(request):
+    context = {}
+    auth_logout(request)
+    try:
+        del request.session['userid']
+    except KeyError:
+        pass
+
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            request.session['userid'] = username
+            return redirect('dashboard:index')
+    return render(request, 'dashboard/login.html', context)
