ba0e7848788f7c36f76ff4e504cf620504507197
Shi Ping <limshiping93@hotmail.com>
2017-05-27 14:33:37 +0800

    Analytics: added python scripts

--- src/dashboard/models.py
+++ src/dashboard/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
 # Create your models here.
-class OrderSnapshot(models.Model):
+class OrderDifference(models.Model):
     clm_code = models.CharField(max_length=16, default='')
-    sold_to_name = models.CharField(max_length=16, default='')
-    sales_name = models.CharField(max_length=16, default='')
+    soldtoname = models.CharField(max_length=16, default='')
+    salesname = models.CharField(max_length=16, default='')
     monat = models.CharField(max_length=16, default='')
     akt_day = models.DateField()
+    last_umwteuro_amt = models.FloatField(default=0)
+    this_umwteuro_amt = models.FloatField(default=0)
+    diff_umwteuro = models.FloatField(default=0)
+    last_umwtpcs_amt = models.IntegerField(default=0)
+    this_umwtpcs_amt = models.IntegerField(default=0)
+    diff_umwtpcs = models.IntegerField(default=0)
+    diff_umwtpcs_percent = models.FloatField(default=0)
+    sc_diff_umwteuro_percent = models.FloatField(default=0)
+    alert_type = models.CharField(max_length=16, default='')
+    alert_description = models.CharField(max_length=64, default='')
