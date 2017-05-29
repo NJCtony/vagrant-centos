490897c1207ced838a02f883a96722a33f0ea616
Sean <sean_lim@mymail.sutd.edu.sg>
2017-05-30 03:44:51 +0800

    Dashboard (Need1) : Allowed consolidation of company structural changes

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -75,39 +75,40 @@ def need_one(request):
 def api_need_one_records(request):
     monat_list = [temp_dict['monat'] for temp_dict in NeedOneRecord.objects.values('monat').distinct()]
     soldtoname_available = [temp_dict['soldtoname'] for temp_dict in NeedOneRecord.objects.values('soldtoname').distinct()]
-    #TODO: Narrow down soldtoname_list based on CLM's access
+    soldtoname_choices = soldtoname_available
 
     summary = []
-    for soldtoname_item in soldtoname_list:
-        soldtoname_summary = {}
-        sc_mean = [0.0,0.0,0.0]
-        sc_count = [0,0,0]
+    for soldtoname_choice in soldtoname_choices:
+        if soldtoname_choice in soldtoname_available:
+            soldtoname_summary = {}
+            sc_mean = [0.0,0.0,0.0]
+            sc_count = [0,0,0]
 
-        soldtoname_summary['soldtoname'] = soldtoname_item
-        soldtoname_summary['salesname_sc'] = []
-        salesname_list = [temp_dict['salesname'] for temp_dict in NeedOneRecord.objects.filter(soldtoname = soldtoname_item).values('salesname').distinct()]
-        for salesname_item in salesname_list:
-            salesname_sc = [] # set of structural-change-% for each sales-name
-            for monat_item in monat_list:
-                if NeedOneRecord.objects.filter(salesname=salesname_item, monat=monat_item).exists():
-                    salesname_sc.append(NeedOneRecord.objects.filter(salesname=salesname_item, monat=monat_item) \
-                    .values('sc_diff_umwteuro_percent')[0]['sc_diff_umwteuro_percent'])
-                else:
-                    salesname_sc.append(0.0)
+            soldtoname_summary['soldtoname'] = soldtoname_choice
+            soldtoname_summary['salesname_sc'] = []
+            salesname_list = [temp_dict['salesname'] for temp_dict in NeedOneRecord.objects.filter(soldtoname = soldtoname_choice).values('salesname').distinct()]
+            for salesname_item in salesname_list:
+                salesname_sc = [] # set of structural-change-% for each sales-name
+                for monat_item in monat_list:
+                    if NeedOneRecord.objects.filter(salesname=salesname_item, monat=monat_item).exists():
+                        salesname_sc.append(NeedOneRecord.objects.filter(salesname=salesname_item, monat=monat_item) \
+                        .values('sc_diff_umwteuro_percent')[0]['sc_diff_umwteuro_percent'])
+                    else:
+                        salesname_sc.append(0.0)
 
+                for i in range(len(salesname_sc)):
+                    if salesname_sc[i] != 0:
+                        sc_mean[i] += salesname_sc[i]
+                        sc_count[i] += 1
+
+                soldtoname_summary['salesname_sc'].append({'salesname': salesname_item, 'sc': salesname_sc})
             for i in range(len(salesname_sc)):
-                if salesname_sc[i] != 0:
-                    sc_mean[i] += salesname_sc[i]
-                    sc_count[i] += 1
+                if sc_mean[i] > 0:
+                    sc_mean[i] /= sc_count[i]
+                sc_mean[i] = round(sc_mean[i], 1)
 
-            soldtoname_summary['salesname_sc'].append({'salesname': salesname_item, 'sc': salesname_sc})
-        for i in range(len(salesname_sc)):
-            if sc_mean[i] > 0:
-                sc_mean[i] /= sc_count[i]
-            sc_mean[i] = round(sc_mean[i], 1)
-
-        soldtoname_summary['mean'] = sc_mean
-        summary.append(soldtoname_summary)
+            soldtoname_summary['mean'] = sc_mean
+            summary.append(soldtoname_summary)
 
     return JsonResponse({'data' : {'summary': summary, 'label': monat_list}})
 
