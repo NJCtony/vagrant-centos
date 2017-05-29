0b60416b7397a988e0db3670381ee4ef42895cf3
Sean <sean_lim@mymail.sutd.edu.sg>
2017-05-30 03:43:59 +0800

    Dashboard (Need1) : Allowed consolidation of company alerts in alert listing

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -74,7 +74,7 @@ def need_one(request):
 # JSON API Endpoints
 def api_need_one_records(request):
     monat_list = [temp_dict['monat'] for temp_dict in NeedOneRecord.objects.values('monat').distinct()]
-    soldtoname_list = [temp_dict['soldtoname'] for temp_dict in NeedOneRecord.objects.values('soldtoname').distinct()]
+    soldtoname_available = [temp_dict['soldtoname'] for temp_dict in NeedOneRecord.objects.values('soldtoname').distinct()]
     #TODO: Narrow down soldtoname_list based on CLM's access
 
     summary = []
@@ -112,39 +112,50 @@ def api_need_one_records(request):
     return JsonResponse({'data' : {'summary': summary, 'label': monat_list}})
 
 def api_need_one_alerts(request):
-    alert_labels = ('Sold to name', 'Sales name', 'MONAT', 'Description')
-    alert_fields = ('soldtoname','salesname','monat','alert_description', 'diff_umwteuro', 'sc_diff_umwteuro_percent') # Define field to be be shown
+    alert_labels = ('Sales name', 'MONAT', 'Description')
+    alert_fields = ('salesname','monat','alert_description', 'diff_umwteuro', 'sc_diff_umwteuro_percent') # Define field to be be shown
 
-    num_decline_alerts = 2
-    num_increase_alerts = 3
-    decline_alert_count = 0
-    increase_alert_count = 0
+    soldtoname_available = [temp_dict['soldtoname'] for temp_dict in NeedOneRecord.objects.values('soldtoname').distinct()]
+    soldtoname_choices = soldtoname_available
 
-    alerts = []
+    consolidatedAlerts = []
 
-    # values() limits the number of fields that is to be converted to list
-    # alerts_query = NeedOneRecord.objects.filter(alert_type="Need 1").order_by('diff_umwteuro')
-    alerts_query = NeedOneRecord.objects.values(*alert_fields).filter(alert_type="Need 1").order_by('diff_umwteuro')
+    for soldtonameChoice in soldtoname_choices:
+        if soldtonameChoice in soldtoname_available:
 
-    for alert in alerts_query:
-        if alert['diff_umwteuro'] < 0 and decline_alert_count < num_decline_alerts:
-            alerts.append(alert)
-            decline_alert_count += 1
+            records_filtered = NeedOneRecord.objects.filter(soldtoname = soldtonameChoice)
 
-    alerts_query_sorted = sorted(alerts_query, key=lambda k: k['sc_diff_umwteuro_percent'], reverse=True)
+            num_decline_alerts = 2
+            num_increase_alerts = 3
+            decline_alert_count = 0
+            increase_alert_count = 0
 
-    for alert in alerts_query_sorted:
-        if alert['sc_diff_umwteuro_percent'] != 100 and increase_alert_count < num_increase_alerts:
-            alerts.append(alert)
-            increase_alert_count += 1
+            alerts = []
 
-    for alert in alerts:
-        alerts_query_sorted.remove(alert)
+            # values() limits the number of fields that is to be converted to list
+            # alerts_query = NeedOneRecord.objects.filter(alert_type="Need 1").order_by('diff_umwteuro')
+            alerts_query = records_filtered.values(*alert_fields).filter(alert_type="Need 1").order_by('diff_umwteuro')
 
-    for alert in alerts_query_sorted:
-        alerts.append(alert)
+            for alert in alerts_query:
+                if alert['diff_umwteuro'] < 0 and decline_alert_count < num_decline_alerts:
+                    alerts.append(alert)
+                    decline_alert_count += 1
 
-    return JsonResponse({'data' : list(alerts), 'labels' : alert_labels})
+            alerts_query_sorted = sorted(alerts_query, key=lambda k: k['sc_diff_umwteuro_percent'], reverse=True)
+
+            for alert in alerts_query_sorted:
+                if alert['sc_diff_umwteuro_percent'] != 100 and increase_alert_count < num_increase_alerts:
+                    alerts.append(alert)
+                    increase_alert_count += 1
+
+            for alert in alerts:
+                alerts_query_sorted.remove(alert)
+
+            for alert in alerts_query_sorted:
+                alerts.append(alert)
+
+        consolidatedAlerts.append({'soldtoname': soldtonameChoice, 'alerts': alerts})
+    return JsonResponse({'data' : {'listing': consolidatedAlerts, 'labels' : alert_labels}})
 
 def template_need_one(request):
     template = loader.get_template('dashboard/template_need1.html')
